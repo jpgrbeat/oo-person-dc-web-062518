b0b1fb8bd0c6617b6664b4b10eb5b7afef90f0c7
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-27 19:57:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,13 @@
 require 'pry'
 class Person
   attr_accessor :bank_account
-  attr_reader :name, :happiness
+  attr_reader :name, :happiness :hygiene
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
+    @hygiene = 8
+    
     # binding.pry
   end
   
