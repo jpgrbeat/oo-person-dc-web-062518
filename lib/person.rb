73fcc3d003f3c43956b928f3f4799fe1580fb49c
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-27 19:28:04 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,14 +4,16 @@ class Person
   attr_reader :name
   def initialize(name)
     @name = name
+    @bank_account = 25
+    
   end
   
-  def bank_account=(bank_account = 25)
-    @bank_account = bank_account
-    binding.pry
-  end
+  # def bank_account=(bank_account = 25)
+  #   @bank_account = bank_account
+  #   binding.pry
+  # end
   
-  def bank_account
-    @bank_account
-  end
+  # def bank_account
+  #   @bank_account
+  # end
 end
