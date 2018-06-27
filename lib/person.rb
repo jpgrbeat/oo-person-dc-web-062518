4e09ceab3b058df937fea2871735d4f73a418ec8
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-27 21:13:45 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -42,4 +42,61 @@ class Person
      false
    end
   end
+  
+  
+
+ def helper?(rating)
+   if rating > 7
+       true
+     else
+       false
+     end
+ end
+
+ def happy?
+   helper?(@happiness)
+
+ end
+
+ def clean?
+   helper?(@hygiene)
+ end
+
+ def get_paid(salary)
+   @bank_account += salary
+   return “all about the benjamins”
+ end
+
+ def take_bath
+   self.hygiene += 4
+   “♪ Rub-a-dub just relaxing in the tub ♫”
+ end
+
+ def work_out
+   self.hygiene -= 3
+   self.happiness += 2
+   “♪ another one bites the dust ♫”
+ end
+
+ def call_friend(person)
+   self.happiness += 3
+   person.happiness += 3
+   “Hi #{person.name}! It’s #{self.name}. How are you?”
+ end
+
+ def start_conversation(person, topic)
+   if topic == “politics”
+     self.happiness -= 2
+     person.happiness -= 2
+     “blah blah partisan blah lobbyist”
+   elsif topic == “weather”
+     self.happiness += 1
+     person.happiness += 1
+     “blah blah sun blah rain”
+   else
+     “blah blah blah blah blah”  
+   end
+ end
+
+end
 end
